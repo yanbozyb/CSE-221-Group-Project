3284c60723b76cb31fd4f77e0358ff6806b5d9e9
Yanbo <yanbo.zyb@outlook.com>
2023-11-12 20:24:55 -0800

    update code

--- sourcecode/memory/ram_access_time.c
+++ sourcecode/memory/ram_access_time.c
@@ -9,7 +9,6 @@
 #define MAX_MEM_SIZE 8 * 1024 * 16384
 #define STRIP 256
 
-// This function reads the current value of the processor's time-stamp counter.
 static inline uint64_t rdtsc() {
     uint32_t lo, hi;
     __asm__ __volatile__ (
@@ -22,20 +21,20 @@ int main() {
     int i, j, tmp;
     size_t size, strip = STRIP;
     int *chain_list;
-    size_t memsize = MIN_MEM_SIZE; // Start with the minimum memory size
+    size_t memsize = MIN_MEM_SIZE;
 
-    const double cpu_freq = 2.9e9; // Replace with your CPU frequency in Hz
+    const double cpu_freq = 2.9e9; // curren cpu frequency
 
-    // Allocate and access memory regions of increasing size to find the latency
+    // allocate and access memory regions of increasing size to find the latency
     for (; memsize <= (MAX_MEM_SIZE); memsize *= 2) {
-        // Map memory to prevent it from being allocated in the swap
+        // map memory to prevent it from being allocated in the swap
         char* segment = mmap(NULL, memsize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
         if (segment == MAP_FAILED) {
             perror("mmap");
             exit(EXIT_FAILURE);
         }
 
-        // Allocate chain_list array
+        // allocate chain_list array
         size = memsize / strip;
         chain_list = malloc(size * sizeof(int));
         if (!chain_list) {
@@ -43,7 +42,7 @@ int main() {
             exit(EXIT_FAILURE);
         }
 
-        // Initialize chain_list with random values to simulate random access
+        // initialize chain_list with random values to simulate random access
         for (i = 0; i < size; i++)
             chain_list[i] = i;
 
@@ -54,26 +53,26 @@ int main() {
             chain_list[j] = tmp;
         }
 
-        // Fill memory with pointer references to create a chain
+        // fill memory with pointer references to create a chain
         for (i = 0; i < size - 1; i++)
             *(char **)&segment[chain_list[i] * strip] = (char*)&segment[chain_list[i + 1] * strip];
         *(char **)&segment[chain_list[size - 1] * strip] = (char*)&segment[chain_list[0] * strip];
 
         register char **p = (char **)segment;
 
-        // Measure access time using rdtsc
+        // measure access time using rdtsc
         uint64_t start_cycles = rdtsc();
         tmp = size * 100;
         for (i = 0; i < tmp; i++) {
-            OPERATION; // Use the macro to minimize loop overhead
+            OPERATION;
         }
         uint64_t end_cycles = rdtsc();
 
-        // Prevent potential optimization
+        // prevent potential optimization
         char **pp = p;
         (void)pp;
 
-        // Calculate the number of cycles and convert to time
+        // calculate the number of cycles and convert to time
         uint64_t cycles = end_cycles - start_cycles;
         double time_diff = cycles / cpu_freq; // seconds
         double latency = (time_diff / tmp) * 1e9; // convert to nanoseconds
@@ -83,7 +82,6 @@ int main() {
                 memsize / 1024, cycles, time_diff, latency);
         }
 
-        // Clean up
         munmap(segment, memsize);
         free(chain_list);
     }
