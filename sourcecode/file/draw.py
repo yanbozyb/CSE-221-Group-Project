d891bfed325682d522eb0238e63c17b8ab70f31b
Yanbo <yanbo.zyb@outlook.com>
2023-12-07 15:46:30 -0800

    add figure

--- sourcecode/file/draw.py
+++ sourcecode/file/draw.py
@@ -2,24 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = """
-buffer: 1 KB, cycles 1732, time 0.000001 s, latency 1.493103 ns
-buffer: 2 KB, cycles 3662, time 0.000001 s, latency 1.578448 ns
-buffer: 4 KB, cycles 7254, time 0.000003 s, latency 1.563362 ns
-buffer: 8 KB, cycles 14556, time 0.000005 s, latency 1.568534 ns
-buffer: 16 KB, cycles 29340, time 0.000010 s, latency 1.580819 ns
-buffer: 32 KB, cycles 92022, time 0.000032 s, latency 2.479041 ns
-buffer: 64 KB, cycles 295084, time 0.000102 s, latency 3.974731 ns
-buffer: 128 KB, cycles 629562, time 0.000217 s, latency 4.240046 ns
-buffer: 256 KB, cycles 1145362, time 0.000395 s, latency 3.856957 ns
-buffer: 512 KB, cycles 3180086, time 0.001097 s, latency 5.354401 ns
-buffer: 1024 KB, cycles 14036716, time 0.004840 s, latency 11.817009 ns
-buffer: 2048 KB, cycles 58921684, time 0.020318 s, latency 24.802029 ns
-buffer: 4096 KB, cycles 106146366, time 0.036602 s, latency 24.340207 ns
-buffer: 8192 KB, cycles 239645246, time 0.082636 s, latency 25.218595 ns
-buffer: 16384 KB, cycles 1837619304, time 0.633662 s, latency 96.689122 ns
-buffer: 32768 KB, cycles 4034712676, time 1.391280 s, latency 106.146258 ns
-buffer: 65536 KB, cycles 7747148560, time 2.671431 s, latency 101.906988 ns
-buffer: 131072 KB, cycles 17066603888, time 5.885036 s, latency 112.248150 ns
+File size: 1 KB, Read time: 3.113 ms
+File size: 2 KB, Read time: 0.004 ms
+File size: 4 KB, Read time: 0.001 ms
+File size: 8 KB, Read time: 0.002 ms
+File size: 16 KB, Read time: 0.002 ms
+File size: 32 KB, Read time: 0.449 ms
+File size: 64 KB, Read time: 1.467 ms
+File size: 128 KB, Read time: 0.077 ms
+File size: 256 KB, Read time: 12.135 ms
+File size: 512 KB, Read time: 6.620 ms
+File size: 1024 KB, Read time: 2.282 ms
+File size: 2048 KB, Read time: 0.784 ms
+File size: 4096 KB, Read time: 5.726 ms
+File size: 8192 KB, Read time: 3.005 ms
+File size: 16384 KB, Read time: 15.209 ms
+File size: 32768 KB, Read time: 20.254 ms
+File size: 65536 KB, Read time: 43.785 ms
+File size: 131072 KB, Read time: 97.094 ms
+File size: 262144 KB, Read time: 1050.603 ms
+File size: 524288 KB, Read time: 2317.302 ms
+File size: 1048576 KB, Read time: 4635.801 ms
 """
 
 data2= """
@@ -46,31 +49,50 @@ File size: 524288 KB, Read time: 206.188 ms
 File size: 1048576 KB, Read time: 408.917 ms
 """
 
-sizes = []
-latencies = []
-for line in data2.strip().split('\n'):
-    parts = line.split(',')
-    size_part = parts[0]  # "buffer: 1 KB"
-    latency_part = parts[3]  # "latency 1.556897 ns"
+def parse_data(dataset):
+    sizes = []
+    latencies = []
+    for line in dataset.strip().split('\n'):
+        parts = line.split(',')
+        size_kb = int(parts[0].split()[2])
+        latency_ms = float(parts[1].split()[2])
+        sizes.append(np.log2(size_kb))
+        latencies.append(latency_ms)
+    return sizes, latencies
 
-    size_kb = int(size_part.split()[1])
-    latency_ms = float(latency_part.split()[1])
-
-    sizes.append(np.log2(size_kb))
-    latencies.append(latency_ms)
+sizes1, latencies1 = parse_data(data)
+sizes2, latencies2 = parse_data(data2)
 
 # draw figure
-fig = plt.figure(figsize=(5.5, 3.2))
-plt.plot(sizes, latencies, marker='o')
-plt.xlabel('File Access Size (KiB))')
-plt.ylabel('Average Latency (ms)')
-plt.title('File Access Latency')
-xtick_labels = [f'{int(size)}' for size in sizes]
-plt.xticks(sizes, xtick_labels, rotation=0)
-plt.ylim([0, 140])
+# Split the data for two subplots
+split_index = 15  # Choose an appropriate index to split the data
+sizes1_left, sizes1_right = sizes1[:split_index], sizes1[split_index:]
+sizes2_left, sizes2_right = sizes2[:split_index], sizes2[split_index:]
+latencies1_left, latencies1_right = latencies1[:split_index], latencies1[split_index:]
+latencies2_left, latencies2_right = latencies2[:split_index], latencies2[split_index:]
 
+# Plotting
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
 
-#plt.grid(True)
-fig.tight_layout()
-plt.savefig('access_latency.pdf',dpi=300)
+# Left subplot for smaller sizes
+ax1.plot(sizes1_left, latencies1_left, marker='o', label='First Access')
+ax1.plot(sizes2_left, latencies2_left, marker='x', label='Full Cached Access')
+ax1.set_xlabel('Log2 (File Access Size (KiB))')
+ax1.set_ylabel('Average Latency (ms)')
+ax1.set_title('(a) Smaller File Sizes')
+ax1.legend()
+
+# Right subplot for larger sizes
+ax2.plot(sizes1_right, latencies1_right, marker='o', label='First Access')
+ax2.plot(sizes2_right, latencies2_right, marker='x', label='Full Cached Access')
+ax2.set_xlabel('Log2 (File Access Size (KiB))')
+#ax2.set_ylabel('Average Latency (ms)')
+ax2.set_title('(b) Larger File Sizes')
+ax2.legend()
+
+#plt.ylim([0, 140])
+plt.tight_layout()
+plt.savefig('filecache.pdf',dpi=300)
 plt.show()
+
+
